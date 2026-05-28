3d090de6646224489be39f693aad994d4c45a494
huntcl <huntcl@oregonstate.edu>
2026-05-27 23:39:53 -0700

    Delete old_work directory

--- old_work/animal.py
+++
@@ -1,8 +0,0 @@
-# Author: Clara Hunt
-# GitHub username: huntcl
-# Date: 03/31/2026
-# Description: Asks the user for their favorite animal and then
-#              prints out that that is their favorite animal
-
-fave_animal = input("Please enter your favorite animal: ")
-print("Your favorite animal is the " + fave_animal + ".")
--- old_work/fall_distance.py
+++
@@ -1,8 +0,0 @@
-# Author: Clara Hunt
-# Github username: huntcl
-# Date: 04/15/26
-# Description: Calculates the distance an object falls for a given time in seconds.
-
-def fall_distance(time):
-    """Return the distance an object falls in time t seconds."""
-    return 0.5 * 9.8 * (time ** 2)
